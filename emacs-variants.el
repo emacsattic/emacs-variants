3f26c2aaec592078536b869bb8cbffb4dfc9e634
Noah Friedman <friedman@splode.com>
2016-12-08 01:04:33 +0100

    updates

--- emacs-variants.el
+++ emacs-variants.el
@@ -1,26 +1,9 @@
 ;;; emacs-variants.el --- identify emacs versions, variants, and platforms
 
-;; Copyright (C) 1991-1999 Noah S. Friedman
-
 ;; Author: Noah Friedman <friedman@splode.com>
-;; Maintainer: friedman@splode.com
+;; Public domain
 
-;; $Id: emacs-variants.el,v 1.1 1999/10/11 08:21:20 friedman Exp $
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, you can either send email to this
-;; program's maintainer or write to: The Free Software Foundation,
-;; Inc.; 59 Temple Place, Suite 330; Boston, MA 02111-1307, USA.
+;; $Id: emacs-variants.el,v 1.2 2016/11/24 20:24:52 friedman Exp $
 
 ;;; Commentary:
 ;;; Code:
@@ -92,9 +75,9 @@ Examples:
     (unwind-protect
 	(and (string-match regexp emacs-version)
 	     (setq version
-		   (string-to-int (substring emacs-version
-					     (match-beginning 1)
-					     (match-end 1)))))
+		   (string-to-number (substring emacs-version
+                                                (match-beginning 1)
+                                                (match-end 1)))))
       (store-match-data old-match-data))
     version))
 
